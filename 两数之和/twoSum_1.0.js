4ad4c61d2ee57fa58ffbe6592a7684b3b8757dac
RedHamo <1043172934@qq.com>
2022-05-29 09:13:00 +0800

    测试提交

---
+++ 两数之和/twoSum_1.0.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let hash = {[nums[0]]:0};
+    for (let index = 1; index < nums.length; index++) {
+        if ( hash[target - nums[index]] >= 0) {
+            return [hash[target - nums[index]], index]
+        }else{
+            hash[nums[index]] = index;
+        }
+    }
+};
+// @lc code=end
+
