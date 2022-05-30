85863b54b50b37cbe166ec7e4929c9f521f93ee2
RedHamo <1043172934@qq.com>
2022-05-31 07:50:13 +0800

    Re:从零开始的力扣生活

---
+++ 2235.两整数相加/sum.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=2235 lang=javascript
+ *
+ * [2235] 两整数相加
+ */
+
+// @lc code=start
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+// Re:从零开始的力扣生活
+ var sum = function(num1, num2) {
+    return num1 + num2
+};
+// @lc code=end
+
