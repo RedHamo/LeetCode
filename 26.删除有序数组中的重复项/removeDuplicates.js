8c2ff4b29f89f4de067de6f045f7413e2ef8a071
quan.yiqiang <1043172934@qq.com>
2022-06-05 07:06:08 +0800

    26代码优化

--- 26.删除有序数组中的重复项/removeDuplicates.js
+++ 26.删除有序数组中的重复项/removeDuplicates.js
@@ -13,10 +13,9 @@
     if(nums.length == 0) return 0;
     let i = 0;
     for(let j = 1; j < nums.length; j++){
-        if(nums[j] !== nums[i]){
-            i++;
-            nums[i] = nums[j];
-        }
+        if(nums[j] == nums[i]) continue;
+        i++;
+        nums[i] = nums[j];
     }
     return i + 1;
 };
