97b03d499fe52a6aa8ca919a23787a3aae98936c
quan.yiqiang <1043172934@qq.com>
2022-06-22 04:21:37 +0800

    改进

--- 35.搜索插入位置/searchInsert.js
+++ 35.搜索插入位置/searchInsert.js
@@ -11,15 +11,21 @@
  * @return {number}
  */
  var searchInsert = function(nums, target) {
-    console.log(nums)
-    if(nums.length==0) return 0;
-    if(nums.length==1 && nums[0]==target) return 0;
-    if(nums.length==1) return nums[0]<target?1:0;
-    let med = ~~(nums.length/2);
-    if(nums[med] == target) return med
-    if(nums[med] > target) return searchInsert(nums.splice(0,med),target)
-    if(nums[med] < target) return med + searchInsert(nums.splice(med,nums.length-med),target)
-    return never;
+    let l=0,
+    r=nums.length-1,
+    ans = nums.length;
+    while (l<=r) {
+        let mid = ((r-l)>>1) + l;
+        if (target == nums[mid]) {
+            return mid;
+        }else if (target < nums[mid]) {
+            ans = mid;
+            r = mid - 1;
+        }else{
+            l = mid + 1;
+        }
+    }
+    return ans;
 };
 // @lc code=end
 
---
+++ 35.搜索插入位置/searchInsert1.0.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=javascript
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var searchInsert = function(nums, target) {
+    if(nums.length==0) return 0;
+    if(nums.length==1 && nums[0]==target) return 0;
+    if(nums.length==1) return nums[0]<target?1:0;
+    let med = ~~(nums.length/2);
+    if(nums[med] == target) return med
+    if(nums[med] > target) return searchInsert(nums.splice(0,med),target)
+    if(nums[med] < target) return med + searchInsert(nums.splice(med,nums.length-med),target)
+    return never;
+};
+// @lc code=end
+
