fe82ba6e126d6eb3d29e3972bcacfeb0d9fd68f6
quan.yiqiang <1043172934@qq.com>
2022-06-25 09:56:56 +0800

    添加注释

--- 14.最长公共前缀/longestCommonPrefix.js
+++ 14.最长公共前缀/longestCommonPrefix.js
@@ -14,11 +14,14 @@
 // 空间复杂度：O(1)
 var longestCommonPrefix = function(strs) {
     if (strs.length == 0) return "";
-    const rows = strs.length;
-    const cols = strs[0].length;
+    const rows = strs.length;//数组长度
+    const cols = strs[0].length;//第一个字符串长度
     for (let i = 0; i < cols; i++) {
-        const firstChar = strs[0][i];
+        const firstChar = strs[0][i];//第一个字符串的第i个字符
         for (let j = 1; j < rows; j++) {
+            //判断其他字符串的第i个字符
+                //1.是否是最后一个字符
+                //2.i位置的字符是否不一致
             if (strs[j].length == i || strs[j][i] != firstChar) {
                 return strs[0].substring(0, i);
             }
