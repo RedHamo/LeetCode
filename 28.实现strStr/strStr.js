ed749c634f448f24722fdca2de66da1ed3f944a8
quan.yiqiang <1043172934@qq.com>
2022-06-28 07:48:20 +0800

    kmp

--- 28.实现strStr/strStr.js
+++ 28.实现strStr/strStr.js
@@ -1,20 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=javascript
+ *
+ * [28] 实现 strStr()
+ */
+
+// @lc code=start
 /**
- * 在同事家编写
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
  */
+// TODO 需要理解
 var strStr = function(haystack, needle) {
-    if(needle=="") return 0;
-    for(let i=0; i<haystack.length; i++){
-        if(haystack[i]!=needle[0]) continue;
-        let flag = true;
-        for(let j=1; j<needle.length; j++){
-            if(haystack[i+j]==needle[j]) continue;
-            flag = false;
-            break;
+    const n = haystack.length, m = needle.length;
+    if (m === 0) {
+        return 0;
+    }
+    const pi = new Array(m).fill(0);
+    for (let i = 1, j = 0; i < m; i++) {
+        while (j > 0 && needle[i] !== needle[j]) {
+            j = pi[j - 1];
         }
-        if (flag) return i;
+        if (needle[i] == needle[j]) {
+            j++;
+        }
+        pi[i] = j;
+    }
+    for (let i = 0, j = 0; i < n; i++) {
+        while (j > 0 && haystack[i] != needle[j]) {
+            j = pi[j - 1];
+        }
+        if (haystack[i] == needle[j]) {
+            j++;
+        }
+        if (j === m) {
+            return i - m + 1;
+        }
     }
     return -1;
 };
+// @lc code=end
+
