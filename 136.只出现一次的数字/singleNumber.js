556474f2f52dddac3b311b7f8c9f76250433eab5
quan.yiqiang <1043172934@qq.com>
2022-07-07 05:29:31 +0800

    异或运算

---
+++ 136.只出现一次的数字/singleNumber.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=javascript
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var singleNumber = function(nums) {
+    return nums.reduce((a,b) => a^b,0)
+};
+// @lc code=end
+
