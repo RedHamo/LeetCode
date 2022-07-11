196ef9fd2af4aa86df617f0ad6d7c70f091bdd86
quan.yiqiang <1043172934@qq.com>
2022-07-12 06:42:16 +0800

    712

---
+++ 160.相交链表/getIntersectionNode.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=javascript
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    let map = new Map();
+    let a = headA,b = headB;
+    while(a){
+        map.set(a, true);
+        a = a.next;
+    }
+    while(b){
+        if(map.has(b)) return b;
+        b = b.next;
+    }
+    return null;
+};
+// @lc code=end
+
+
