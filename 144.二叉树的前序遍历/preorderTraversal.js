18657ba93a579df55e4a184e2efce13751725829
quan.yiqiang <1043172934@qq.com>
2022-07-13 18:18:54 +0800

    713

---
+++ 144.二叉树的前序遍历/preorderTraversal.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=javascript
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    let ret = []
+    const preorder = (node)=>{
+        if(node==null) return;
+        ret.push(node.val)
+        preorder(node.left)
+        preorder(node.right)
+    }
+    preorder(root)
+    return ret;
+};
+// @lc code=end
+
+
