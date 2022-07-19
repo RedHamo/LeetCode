1e0463a56f21fe84cd7cce95daf6c9fd8e0d8da3
quan.yiqiang <1043172934@qq.com>
2022-07-19 23:16:46 +0800

    719

---
+++ 19.删除链表的倒数第N个结点/removeNthFromEnd.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=javascript
+ *
+ * [19] 删除链表的倒数第 N 个结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    let dummy = new ListNode();
+    dummy.next = head;
+    let n1 = dummy;
+    let n2 = dummy;
+    for (let i = 0; i <= n; i++) {//n2移动n+1次
+        n2 = n2.next;
+    }
+    while (n2 !== null) {//同时移动n1，n2
+        n1 = n1.next;
+        n2 = n2.next;
+    }
+    n1.next = n1.next.next;//删除元素
+    return dummy.next;
+};
+// @lc code=end
+
+
