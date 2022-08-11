f5f968dfd1b331f738217fb7e1fc2caa064a1d07
redhamo <1043172934@qq.com>
2022-08-11 22:41:38 +0800

    巴什博弈

---
+++ 292.Nim游戏/canWinNim.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=292 lang=javascript
+ *
+ * [292] Nim 游戏
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function(n) {
+    // 巴什博弈 Bash Game
+    return n%4!=0;
+};
+// @lc code=end
+
+
