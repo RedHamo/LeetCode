ac0d37029c0c9697acaac30945f519928b0bd614
redhamo <1043172934@qq.com>
2022-08-13 00:05:13 +0800

    m

---
+++ 303.区域和检索-数组不可变/NumArray.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=javascript
+ *
+ * [303] 区域和检索 - 数组不可变
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.nums = nums;
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+    let ret = 0;
+    for(const i in this.nums){
+        if(i<left) continue;
+        if(i>right) return ret;
+        ret+=this.nums[i];
+    }
+    return ret;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
+// @lc code=end
+
+
