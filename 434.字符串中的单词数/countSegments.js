45b4838b19cd33978f3b63d976e590eb3d78cac5
redhamo <1043172934@qq.com>
2022-08-23 08:05:06 +0800

    m

---
+++ 434.字符串中的单词数/countSegments.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=434 lang=javascript
+ *
+ * [434] 字符串中的单词数
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function(s) {
+    return s.split(" ").filter(o=>o).length;
+};
+// @lc code=end
+
+
