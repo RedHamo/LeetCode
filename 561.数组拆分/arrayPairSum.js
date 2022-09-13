d7e646d2216ef37ca454f84976d0aee5f75a7b7d
redhamo <1043172934@qq.com>
2022-09-13 20:40:09 +0800

    m

--- 561.数组拆分/arrayPairSum.js
+++ 561.数组拆分/arrayPairSum.js
@@ -1,6 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=561 lang=javascript
+ *
+ * [561] 数组拆分
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
     nums.sort((a,b)=>b-a)
     let sum = 0;
     for(let i=0; i<nums.length; i++){
         if(i%2==1) sum+=nums[i]
     }
     return sum;
+};
+// @lc code=end
