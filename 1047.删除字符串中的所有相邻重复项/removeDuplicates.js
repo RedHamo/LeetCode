731927b1c054d605923bcfa9e812283824ace262
redhamo <1043172934@qq.com>
2022-12-09 19:07:06 +0800

    m

---
+++ 1047.删除字符串中的所有相邻重复项/removeDuplicates.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function(s) {
+    let stack = []
+    for(let i=0; i<s.length; i++){
+        if(stack.length>0 && stack[stack.length-1]==s[i]) {
+            stack.pop()
+        } else {
+            stack.push(s[i])
+        }
+    }
+    return stack.join("")
+};
