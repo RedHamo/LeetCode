3872dfd964797d4d45ea34dd4ba54c465618cbcd
redhamo <1043172934@qq.com>
2022-12-12 07:57:42 +0800

    m

--- 33.搜索旋转排序数组/search.js
+++ 33.搜索旋转排序数组/search.js
@@ -10,6 +10,8 @@ var search = function(nums, target) {
     helper(0, nums.length-1)
     return result;
     function helper(l,r){
+        // 减枝，如果该部分是有序的，并且target不在该范围，则不往下继续进行
+        if(nums[l]<nums[r] && !(nums[l] <= target && target<=nums[r])) return
         let mid = ~~((l+r)/2);
         if(nums[l]==target) return result=l;
         if(nums[r]==target) return result=r;
