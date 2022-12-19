2510df90538a28188327962d67373f33bd66a9b5
redhamo <1043172934@qq.com>
2022-12-19 12:57:56 +0800

    m

--- 43.字符串相乘/multiply.js
+++ 43.字符串相乘/multiply.js
@@ -12,15 +12,22 @@ var multiply = function(num1, num2) {
             // 从个位开始，逐步相乘
             const mul = num1[i]*num2[j]
             // 乘积在结果数组中对应的位置
+            /**
+             * 123 * 123 
+             = 123*3 + 123*20 + 123*100
+             = 100*3 + 20*3 + 3*3 + ...
+             * 两个个位数相乘，其结果肯定在两位及两位以内
+             * p1是前一位位，p2是当前位位置
+             */
+            
             const p1 = i+j,p2 = i+j+1
-            // 对结果进行累加
+            // 对结果进行累加，重新计算个位和十位的值
             const sum = mul + res[p2]
             res[p1] += (~~(sum/10))
             res[p2] = sum%10
         }
     }
-    console.log(res)
+    // 当前两位乘积不足10的时候，需要将计算结果前部去零
     if(res[0]===0) res.shift()
     return res.join("");
 };
-
