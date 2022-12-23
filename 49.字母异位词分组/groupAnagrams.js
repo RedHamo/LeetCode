ce3d0d92cf1336f6847318fa794d954468fe3dae
redhamo <1043172934@qq.com>
2022-12-23 14:10:18 +0800

    m

---
+++ 49.字母异位词分组/groupAnagrams.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let hashs = {}
+    for(let str of strs){
+        let hash = gethash(str)
+        hashs[hash]?hashs[hash].push(str):hashs[hash]=[str]
+    }
+    return Object.values(hashs)
+};
+
+function gethash(str){
+    return str.split("").sort((a,b)=>a.charCodeAt()-b.charCodeAt()).join("")
+}
